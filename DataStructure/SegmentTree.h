53f4b58cc4d8ac8f9d713c11e6b2eb3235b2cf2d
UnboxTheCat <evanhyd2003@gmail.com>
2022-04-25 02:24:38 -0400

    Removed the initial value requirement from segment tree's predicate class.

--- DataStructure/SegmentTree.h
+++ DataStructure/SegmentTree.h
@@ -8,31 +8,8 @@
 
 using namespace cug::io;
 
-template <typename BinOp, typename T>
-concept binary_predicate = requires(BinOp op, T t)
-{
-    std::same_as<decltype(BinOp::init_value), T>;
-    { op.operator()(t, t) } -> std::same_as<T>;
-};
 
-
-//EXAMPLE OF A BINARY OPERATOR
-//struct Sum
-//{
-//    //initial value of the query result
-//    static const int init_value = 0;
-//
-//    //binary operator for the segments
-//    int operator()(int a, int b)
-//    {
-//        return a + b;
-//    }
-//};
-
-
-
-
-template <typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 class SegmentTree
 {
     std::vector<T> tree_;
@@ -52,14 +29,14 @@ private:
 };
 
 
-template <typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 SegmentTree<T, BinOp>::SegmentTree(std::initializer_list<T> lst) : tree_(lst.size() * 2)
 {
     std::copy(lst.begin(), lst.end(), tree_.begin() + tree_.size() / 2);
     this->BuildParent();
 }
 
-template <typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 SegmentTree<T, BinOp>::SegmentTree(const std::vector<T>& lst) : tree_(lst.size() * 2)
 {
     std::copy(lst.begin(), lst.end(), tree_.begin() + tree_.size() / 2);
@@ -67,7 +44,7 @@ SegmentTree<T, BinOp>::SegmentTree(const std::vector<T>& lst) : tree_(lst.size()
 }
 
 
-template <typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 void SegmentTree<T, BinOp>::PrintTree(int width) const
 {
     //notice the log2 and pow 2 does not cancel out each other due to int trancation
@@ -101,21 +78,32 @@ void SegmentTree<T, BinOp>::PrintTree(int width) const
     }
 }
 
-template <typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 T SegmentTree<T, BinOp>::Query(int left, int right, BinOp op) const
 {
-    T res = BinOp::init_value;
-    for (left += static_cast<int>(tree_.size()) / 2, right += static_cast<int>(tree_.size()) / 2;
-        left <= right; left /= 2, right /= 2)
+    //convert to the child leaf index
+    left += static_cast<int>(tree_.size()) / 2;
+    right += static_cast<int>(tree_.size()) / 2;
+
+    //set up the initial query value
+    T res = tree_[left];
+
+    //check if left and right borders collide
+    while(left <= right)
     {
+        //apply the operators to the edge segment
         if (left % 2 == 1) res = op(res, tree_[left++]);
         if (right % 2 == 0) res = op(res, tree_[right--]);
+
+        //climb up the tree
+        left /= 2;
+        right /= 2;
     }
 
     return res;
 }
 
-template<typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 void SegmentTree<T, BinOp>::Update(int segment, const T& new_value, BinOp op)
 {
     segment += static_cast<int>(tree_.size()) / 2;
@@ -127,7 +115,7 @@ void SegmentTree<T, BinOp>::Update(int segment, const T& new_value, BinOp op)
     }
 }
 
-template<typename T, binary_predicate<T> BinOp>
+template <typename T, typename BinOp>
 void SegmentTree<T, BinOp>::BuildParent(BinOp op)
 {
     for (int i = static_cast<int>(tree_.size()) / 2 - 1; i >= 1; --i)
